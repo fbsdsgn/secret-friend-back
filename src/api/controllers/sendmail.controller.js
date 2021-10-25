e227433b7351b19ef8b65e949b48ad6dcf484d0f
Felipe Souza <fbsdsgn@gmail.com>
2021-10-25 08:16:55 -0300

    update code

--- src/api/controllers/sendmail.controller.js
+++ src/api/controllers/sendmail.controller.js
@@ -1,29 +1,34 @@
-const Friend = require('../../models/friends')
-const nodemailer = require('nodemailer');
-const CONFIG_SMTP = require('../../config/smtp')
+const Friend = require("../../models/friends");
+const nodemailer = require("nodemailer");
+const CONFIG_SMTP = require("../../config/smtp");
 
 module.exports.send = async (req, res) => {
   const friends = await Friend.findAll();
-
-  const randomFriends = await Math.floor(Math.random() * (friends.length - 0 + 1)) + 0;
-
-  const name = friends[randomFriends].dataValues.name
 
   var transport = nodemailer.createTransport({
     host: CONFIG_SMTP.host,
     port: CONFIG_SMTP.port,
     auth: {
       user: CONFIG_SMTP.user,
-      pass: CONFIG_SMTP.pass
-    }
-  })
+      pass: CONFIG_SMTP.pass,
+    },
+  });
 
-  let message = await transport.sendMail({
-    from: `"Pessoa teste <pessoa@teste.com>`,
-    to: "teste@teste.com",
-    subject: "Email com Nodemailer",
-    text: "Este um email de teste",
-    html: `<p>Seu amigo secreto é ${name}</p>`
-  })
-  res.send(name)
-}
+  friends.map(
+    ({ dataValues }, index) => {
+      index = Math.floor(Math.random() * friends.length - 0 + 0) + 0;
+      const name = friends[index].dataValues.name;
+      transport.sendMail({
+        from: `<pessoa@teste.com>`,
+        to: dataValues.email,
+        subject: "Seu amigo secreto",
+        text: "Este um email de teste",
+        html: `<p>Seu amigo secreto é ${name}</p>`,
+      });
+    },
+    (err) => {
+      return res.send(err);
+    }
+  );
+  return res.send({ msg: "ok" });
+};
